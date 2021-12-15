b77a526d822c22fd8eb3f09d283ce42acbdc9537
xcodebuild <me@xcodebuild.com>
2021-12-15 19:50:29 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ac73bd7d41a2464b826bf0d7b5059967',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
